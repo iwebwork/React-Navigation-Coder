a80dbc86f0ddfad3f88628b6a0c03c04fb458d92
Igor <iwebwork.adm@gmail.com>
2021-05-10 23:10:17 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {SafeAreaView, Text} from 'react-native'
+import TextoCentral from './components/TextoCentral'
+
+export default props => (
+    <SafeAreaView>
+        <Text>Ola</Text>
+        <TextoCentral>App</TextoCentral>
+    </SafeAreaView>
+)
