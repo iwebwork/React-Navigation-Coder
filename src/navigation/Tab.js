6b1f6aea09d27d83ab8420d3fdd9d8bafcadc8bf
Igor <iwebwork.adm@gmail.com>
2021-05-19 21:59:42 -0300

    Navigation tab

--- src/navigation/Tab.js
+++ src/navigation/Tab.js
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator()
 
@@ -10,16 +11,53 @@ export default props => {
 
     return (
         <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    switch (route.name){
+                        case 'TelaA':
+                            iconName = focused 
+                                ? 'ios-information-circle'
+                                : 'ios-information-circle-outline'
+                            break
+                        case 'TelaB':
+                            iconName = focused 
+                                ? 'ios-information-circle'
+                                : 'ios-information-circle-outline'
+                            break
+                        case 'TelaC':
+                            iconName = focused 
+                                ? 'ios-information-circle'
+                                : 'ios-information-circle-outline'
+                            break
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
             tabBarOptions={{
                 activeTintColor:'blue',
                 inactiveTintColor:'red',
-                labelStyle:{fontSize:30}
+                showLabel:true,
+                labelStyle:{fontSize:20}
             }}
             initialRouteName="TelaC"
         >
-            <Tab.Screen name="TelaA" component={TelaA}></Tab.Screen>
-            <Tab.Screen name="TelaB" component={TelaB}></Tab.Screen>
-            <Tab.Screen name="TelaC" component={TelaC}></Tab.Screen>
+            <Tab.Screen 
+                name="TelaA" 
+                component={TelaA}
+                options={{title:"Home"}}
+            />
+            <Tab.Screen 
+                name="TelaB" component={TelaB}
+                options={{title:"Pagina 1"}}
+            />
+            <Tab.Screen 
+                name="TelaC" component={TelaC}
+                options={{title:"Pagina 2"}}
+            />
         </Tab.Navigator>
     )
 }
